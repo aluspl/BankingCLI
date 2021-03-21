9e846a6f6b1e3491e7dd8429f173c647b4b2a198
Szymon Motyka <szymon.motyka@angrynerds.pl>
2021-03-21 19:31:24 +0100

    Init Commit

---
+++ BankingCLI/Models/Parameters.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace BankingCLI.Models
+{
+    public class Parameters
+    {
+        [Option('a', "amount", Required = true, HelpText ="Loan Amount.")]
+        public decimal Amount { get; set; }
+
+        [Option('d', "duration", Required = true, HelpText = "Duration of loan (in years).")]
+        public int Duration { get; set; }
+    }
+}
---
+++ BankingCLI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BankingCLI.Models;
+using CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BankingCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = ConfigureDI();
+
+            var options = Parser.Default.ParseArguments<Parameters>(args);
+        }
+
+        private static ServiceProvider ConfigureDI()
+        {
+            var serviceProvider = new ServiceCollection()
+                .BuildServiceProvider();
+            return serviceProvider;
+        }
+    }
+}
